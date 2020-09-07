e59adf3a08e8b5e17a660a4ae18b66ec003813da
Somiparno Chattopadhyay <28658223+clueless-skywatcher@users.noreply.github.com>
2020-09-07 21:15:59 +0530

    Added possible solutions to assumptions

--- okhttpparser.py
+++ okhttpparser.py
@@ -29,6 +29,8 @@ class OkHttpParser:
 		self.loglines = []
 		with open(logfile, 'r') as f:
 			for line in f.readlines()[:lines]:
+				# This part here assumes that the HTTP request-response lines
+				# are contained only in lines where "D/OkHttp" is present
 				if 'D/OkHttp' in line:
 					self.loglines.append(line.split('D/OkHttp: ')[1].rstrip())
 
@@ -37,9 +39,15 @@ class OkHttpParser:
 		req_res_start = False
 		x, y = -1, -1
 		for i in range(len(self.loglines)):
+			# This part assumes that each request starts with a line that contains
+			# "-->" at the beginning
+			# Solution: We might want to check where "GET" or "POST" is mentioned
+			# without an "END" in the line
 			if self.loglines[i].startswith("-->") and not req_res_start:
 				x = i
 				req_res_start = True
+			# Instead of using "-->", searching for an "END" in the line would mean
+			# that the request/response is over
 			if self.loglines[i].startswith("<-- END") and req_res_start:
 				y = i
 				req_res_start = False
@@ -56,6 +64,12 @@ class OkHttpParser:
 		req_res_pairs = []
 
 		for req, res in req_res_pair_bounds:
+			# Here is where things might go wrong. It has been assumed that
+			# when splitting the line by space, the 2nd element would give the
+			# HTTP method, and the 3rd would give the HTTP endpoint for the API
+			# call
+			# Solution: Use a regex to detect the HTTP endpoint, use a "GET/POST"
+			# finder to find the HTTP method used
 			req_method = self.loglines[req[0]].split(" ")[1]
 			req_endpoint = self.loglines[req[0]].split(" ")[2]
 
@@ -67,6 +81,11 @@ class OkHttpParser:
 					header_key = self.loglines[i].split(": ", 1)[0]
 					header_val = self.loglines[i].split(": ", 1)[1]
 					req_header[header_key] = header_val
+				# Assumption has been made that the payload might start with "{"
+				# If payload starts with "{" then it has been assumed that the line
+				# is a JSON object, or otherwise a plain payload. This may break for
+				# multipart data or images
+				# Solution: Using a lookahead parser for detecting multipart data/images.
 				if self.loglines[i].startswith("{"):
 					req_payload = json.loads(self.loglines[i])
 				elif not self.loglines[i].startswith("--"):
@@ -74,6 +93,18 @@ class OkHttpParser:
 			request.payload = req_payload
 			request.header = req_header
 
+			# Just like the request parsing, here we are also assuming that the 2nd string
+			# of the split is the status code of a response, the 3rd is the name of the
+			# status code, and the 4th is the endpoint.
+			# Solution: Write a regex for detecting the endpoint, a regex for finding the
+			# status code, and there may be a regex for detecting the name of the status code
+			# (if it exists)
+
+			# Also this may break for image responses, and we are still assuming that strings
+			# beginning with braces are JSON payloads, otherwise they are just strings.
+			# Obviously this would not work properly for images, which are in some sort of
+			# garbled text called "Mojibake" due to incompatible encoding.
+			# Solution: Use proper decoding techniques to decode images.
 			res_status_code = self.loglines[res[0]].split(" ")[1]
 			res_status_code_name = self.loglines[res[0]].split(" ")[2]
 			res_endpoint = self.loglines[res[0]].split(" ")[3]
